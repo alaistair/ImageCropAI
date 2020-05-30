5b9b50f1c8a864ef0db2e4862aa107b45b88cfc1
Alaistair Chan <alaistair@gmail.com>
2020-05-30 20:21:52 +1000

    Init commit. Struct based from Cookiecutter Data Science

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='AI assisted image cropping delivered via Streamlit',
+    author='Alaistair Chan',
+    license='MIT',
+)
